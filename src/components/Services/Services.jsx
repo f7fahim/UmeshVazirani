c72e7c6fd145243e6a3ee367b6551d10a354c94d
mh-akash <akash15-4322@diu.edu.bd>
2022-12-09 21:40:19 +0600

    Initialize repo

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,90 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from './resume.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Education</span>
+        <span> </span>
+        <span> </span>
+        <spane>
+          1986, Ph.D., Computer Science, UC Berkeley
+          <br />
+          1981, B.S., MIT
+        </spane>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Research Areas"}
+            detail={"1. Theory (THY), Quantum computing, algorithms and complexity theory. 2. Security (SEC)"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Research Centers"}
+            detail={"1. Berkeley Quantum Information and Computation Center (BQIC). 2. Simons Institute for the Theory of Computing (SITC)."}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}z
+            heading={"Teaching Schedule"}
+            detail={
+              "1. COMPSCI C191. Quantum Information Science and Technology, TuTh 11:00-12:29, Genetics & Plant Bio 100 2. COMPSCI 294-236. Cryptography in a Quantum World, Mo 14:00-16:59, Hearst Mining"
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
